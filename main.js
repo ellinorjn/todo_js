1a420016b63486649b0251491c14db2db0d1bc94
ellinorjn <ellinor.jansson@medieinstitutet.se>
2018-02-19 18:04:00 +0100

    started over

--- main.js
+++ main.js
@@ -1,25 +1,32 @@
-const inputNewTodo = document.getElementById('inputNewTodo');
-const buttonAddNewTodo = document.getElementById ('buttonAddNewTodo');
+const inputNewTodo = document.getElementById("inputNewTodo");
+const buttonAddNewTodo = document.getElementById("buttonAddNewTodo");
 const buttonDeleteAllTodos = document.getElementById('buttonDeleteAllTodos');
 
-//Add an eventlistener to the adding button
-buttonAddNewTodo.addEventListener('click', function () {
-    //Create li-element and add the inputvalue to the li-element
+//creates a new li-element from the inputfield
+function newListItem() {
     const li = document.createElement("li");
     const inputText = document.getElementById("inputNewTodo").value;
     const theTodo = document.createTextNode(inputText);
+    li.appendChild(theTodo);
+    //Checks if the inputfield is empty or not
     if (inputText === "") {
         alert("You can't add an empty todo, please write a todo");
     } else {
-        li.appendChild(theTodo);
         document.getElementById("ulAllTodos").appendChild(li);
     }
-});
+}
 
+//call newListItem function when clicking on the add todo button
+buttonAddNewTodo.addEventListener('click', function(){
+    newListItem();
+})
 
-//Add an eventlistener to the delete button
-//and remove all the li's inside the ul when clicking 
+/*Add an eventlistener to the delete button
+and remove all the li's inside the ul when clicking*/ 
 buttonDeleteAllTodos.addEventListener('click', function () {
     var todo = document.getElementById("ulAllTodos");
     while (todo.firstChild)todo.removeChild(todo.firstChild);
 });
+
+
+
