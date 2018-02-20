0f298f527392ec3a811ab2ffc5e91b3a47560fe3
ellinorjn <ellinor.jansson@medieinstitutet.se>
2018-02-20 14:33:49 +0100

    added delete and completed button, delete button is working

--- main.js
+++ main.js
@@ -4,52 +4,45 @@ const buttonDeleteAllTodos = document.getElementById('buttonDeleteAllTodos');
 
 //creates a new li-element from the inputfield
 function newListItem() {
-    const li = document.createElement("li");
-    const inputText = document.getElementById("inputNewTodo").value;
-    const theTodo = document.createTextNode(inputText);
-    li.appendChild(theTodo);
-    
-    const buttonDeleteTodo = document.createElement("button");
-    const textButtonDelete = document.createTextNode("Delete");
-    
     //Checks if the inputfield is empty
-    if (inputText === "") {
+    if (inputNewTodo === "") {
         alert("You can't add an empty todo, please write a todo");
     } else {
+        const li = document.createElement("li");
+        li.className = "lista"
+        const inputText = document.getElementById("inputNewTodo").value;
+        const theTodo = document.createTextNode(inputText);
+        li.appendChild(theTodo);
         document.getElementById("ulAllTodos").appendChild(li);
-        buttonDeleteTodo.appendChild(textButtonDelete)
-        li.appendChild(buttonDeleteTodo)
+        deleteTodo(li)
+        completeTodo(li)
     }
 }
 
-/*
-function deleteTodo(){
-    const todos = document.getElementsByTagName('li')
-    for(const todo of todos){
-        let buttonDeleteTodo = document.createElement("button");
-        let textButtonDelete = document.createTextNode("Delete");
-        buttonDeleteTodo.appendChild(textButtonDelete)
-        todo.appendChild(buttonDeleteTodo)
-    }
+
+function deleteTodo(li) {
+    const buttonDeleteTodo = document.createElement("button");
+    buttonDeleteTodo.className = "buttonDeleteTodo"
+    const textButtonDelete = document.createTextNode("Delete");
+    buttonDeleteTodo.appendChild(textButtonDelete)
+    li.appendChild(buttonDeleteTodo)
+    //Gets my ul and removes the child when clicking on delete
+    buttonDeleteTodo.addEventListener('click', function () {
+        var removeTodo = document.getElementById('ulAllTodos')
+        removeTodo.removeChild(li)
+    });
+}
+function completeTodo(li){
+     const buttonCompletedTodo = document.createElement("button");
+        buttonCompletedTodo.className = "buttonCompletedTodo"
+        const textButtonCompleted = document.createTextNode("Completed!")
+        buttonCompletedTodo.appendChild(textButtonCompleted)
+        li.appendChild(buttonCompletedTodo)
 }
 
-*/
-/*
-function deleteTodo(){    
-    var allTodos = document.getElementsByTagName("li");
-    for(let i = 0; i < allTodos.length; i++){
-        let buttonDeleteTodo = document.createElement("button");
-        let textButtonDelete = document.createTextNode("Delete");
-        buttonDeleteTodo.appendChild(textButtonDelete)
-        allTodos[i].appendChild(buttonDeleteTodo)
-    }
-}
-*/
 
 
-
-
-//call newListItem function when clicking on the add todo button
+//call newListItem function when clicking on the add button
 buttonAddNewTodo.addEventListener('click', function(){
     newListItem();
 })
