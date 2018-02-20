13cdb6f1f95ad561b1c254286a54e5f20ac6e4bb
ellinorjn <ellinor.jansson@medieinstitutet.se>
2018-02-20 12:34:56 +0100

    delete button to each todo

--- main.js
+++ main.js
@@ -8,13 +8,46 @@ function newListItem() {
     const inputText = document.getElementById("inputNewTodo").value;
     const theTodo = document.createTextNode(inputText);
     li.appendChild(theTodo);
-    //Checks if the inputfield is empty or not
+    
+    const buttonDeleteTodo = document.createElement("button");
+    const textButtonDelete = document.createTextNode("Delete");
+    
+    //Checks if the inputfield is empty
     if (inputText === "") {
         alert("You can't add an empty todo, please write a todo");
     } else {
         document.getElementById("ulAllTodos").appendChild(li);
+        buttonDeleteTodo.appendChild(textButtonDelete)
+        li.appendChild(buttonDeleteTodo)
     }
 }
+
+/*
+function deleteTodo(){
+    const todos = document.getElementsByTagName('li')
+    for(const todo of todos){
+        let buttonDeleteTodo = document.createElement("button");
+        let textButtonDelete = document.createTextNode("Delete");
+        buttonDeleteTodo.appendChild(textButtonDelete)
+        todo.appendChild(buttonDeleteTodo)
+    }
+}
+
+*/
+/*
+function deleteTodo(){    
+    var allTodos = document.getElementsByTagName("li");
+    for(let i = 0; i < allTodos.length; i++){
+        let buttonDeleteTodo = document.createElement("button");
+        let textButtonDelete = document.createTextNode("Delete");
+        buttonDeleteTodo.appendChild(textButtonDelete)
+        allTodos[i].appendChild(buttonDeleteTodo)
+    }
+}
+*/
+
+
+
 
 //call newListItem function when clicking on the add todo button
 buttonAddNewTodo.addEventListener('click', function(){
